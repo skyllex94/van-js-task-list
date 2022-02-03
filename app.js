2ffd84890284207c392c9caef67e3d1dea6fcac0
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2022-02-03 16:08:26 +0200

    html initializing

---
+++ app.js
@@ -0,0 +1,36 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load All EventListeners
+loadEventListeners();
+
+function loadEventListeners(){
+    // Add Task Event
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(event){
+    if(taskInput.value === ''){
+        alert('Please add a task!');
+    }
+    // Create Li Element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    
+    // Create new link Element
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+
+    //Append written Task to the Task List
+    taskList.appendChild(li);
+    taskInput.value = '';
+
+    event.preventDefault();
+}
